4419e747518a431801b3767b5c5999fb74451256
harshisubhramanyam <86614748+harshisubhramanyam@users.noreply.github.com>
2021-06-29 05:47:51 +0530

    Create Helloworld.java

---
+++ Helloworld.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Helloworld{
+
+  public static void main(String args[]){
+    int i=0;
+    m-sc.next().charAt(0);
+    switch(m)
+    {
+    case'a':
+    case'e':
+    case'i':
+    case'o':
+    case'u':
+    case'A':
+    case'E':
+    case'I':
+    case'O':
+    case'U':i++;
+    }
+    if(i==1)
+    System.out.println("Vowel");
+    else
+    iff((m>='a'&&m<='z')||(m>='A'&&m<='Z'))
+    System.out.println("Consonent");
+    }
+    }
