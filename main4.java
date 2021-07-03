0a377b943c335470973b2271a4d12ef6315891b9
harshisubhramanyam <86614748+harshisubhramanyam@users.noreply.github.com>
2021-07-03 19:27:29 +0530

    Create main4.java

---
+++ main4.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;  
+public class Main3  
+{  
+public static void main(String[] args)  
+{  
+int i, j, rows;  
+Scanner sc = new Scanner(System.in);  
+System.out.print("Enter the number of rows: ");  
+rows = sc.nextInt();          
+for( i=1;i<=rows;i++)
+ {
+   for( j=1;j<=rows-i;j++)
+    {
+       System.out.print(" ");
+         }
+     for( j=1;j<=i*2-1;j++)
+     {
+       System.out.print("*");
+         }
+     System.out.println();
+      } 
+     for( i=rows-1;i>0;i--)
+     {
+      for( j=1;j<=rows-i;j++)
+        {
+      System.out.print(" ");
+       }
+       for( j=1;j<=i*2-1;j++)
+        {
+      System.out.print("*");
+      }
+       System.out.println();
+     }
+   }
+}
