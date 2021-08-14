f5782e4f46f3ee9b7d5246614598fb6acf07fc60
harshisubhramanyam <86614748+harshisubhramanyam@users.noreply.github.com>
2021-08-14 09:51:37 +0530

    Create BinaryTree.java

---
+++ BinaryTree.java
@@ -0,0 +1,23 @@
+class BinaryTree
+{
+    //Function to return a list containing the preorder traversal of the tree.
+    static ArrayList<Integer> preorder(Node root)
+    {
+        ArrayList<Integer> result=new ArrayList<Integer>();
+        preorderTraversal(root,result);
+        return result;
+    }
+    public static void preorderTraversal(Node root,ArrayList<Integer> result)
+    {
+        if(root==null)
+        return;
+        else
+        {
+        System.out.print(root.data+" ");
+        preorderTraversal(root.left,result);
+        preorderTraversal(root.right,result);
+        }
+
+    }
+
+}
